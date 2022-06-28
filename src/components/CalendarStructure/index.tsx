f1b18ba46c852fcae3c4b0a2a227766e3dd1c7a3
Rohan Karmakar <krohan1202@gmail.com>
2022-06-28 13:44:48 +0530

    Initial commit with dayjs & antd

---
+++ src/components/CalendarStructure/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import dayjs from "dayjs";
+import { Button } from "antd";
+
+export default function CalendarStructure() {
+  // console.log(dayjs());
+
+  return (
+    <div>
+      <h1>CalendarStructure</h1>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}

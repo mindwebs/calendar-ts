409be9603c22f6c713d639069f0fc006c5687fdf
Rohan Karmakar <krohan1202@gmail.com>
2022-07-03 21:28:05 +0530

    Update overflow in timeBlocks

--- src/components/Day/index.tsx
+++ src/components/Day/index.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import dayjs from "dayjs";
 import "./Day.css";
 import { Typography, Row, Col, Divider } from "antd";
 
 import EventModal from "../EventModal/EventModal";
+import { AnyRecord } from "dns";
 
 export default function Day({
   day,
@@ -45,6 +46,36 @@ export default function Day({
 
   console.log("Saved Events", savedEvents);
 
+  var allTimesForEachDateDiv: any = useRef(null);
+  // console.log(allTimesForEachDateDiv);
+  const allWithClass = Array.from(
+    document.getElementsByClassName("alltimesForEachDate")
+  );
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrollForTime, setScrollForTime] = useState(false);
+  useEffect(() => {
+    // allTimesForEachDateDiv.current.scrollTop = scrollPosition;
+    const updatePosition = () => {
+      setScrollPosition(allTimesForEachDateDiv.current.scrollTop);
+    };
+
+    allTimesForEachDateDiv.current.addEventListener("scroll", updatePosition);
+
+    return () =>
+      allTimesForEachDateDiv.current.removeEventListener(
+        "scroll",
+        updatePosition
+      );
+  }, []);
+  useEffect(() => {
+    // console.log("Mu", allTimesForEachDateDiv.current!.scrollTop);
+    console.log("allWithClass", allWithClass);
+    allWithClass.forEach((el: any) => {
+      el.scrollTop = scrollPosition;
+    });
+    // allTimesForEachDateDiv.current!.scrollTop = scrollPosition;
+  }, [scrollPosition]);
+  console.log("scrollPosition", scrollPosition);
   return (
     <>
       <div style={{ display: "flex", width: "98%" }}>
@@ -55,16 +86,23 @@ export default function Day({
           <Title level={2} className="dateDiv">
             &nbsp;
           </Title>
-          {timeArr.map((oneTime: number) => {
-            return (
-              <p
-                style={{ margin: "0", borderLeft: "none", height: "4rem" }}
-                className="timeBlocks"
-              >
-                <span className="calendar_timeHeadings">{oneTime}:00</span>
-              </p>
-            );
-          })}
+          <div
+            style={{
+              overflowY: scrollForTime ? "scroll" : "hidden",
+            }}
+            className="alltimesForEachDate"
+          >
+            {timeArr.map((oneTime: number) => {
+              return (
+                <p
+                  style={{ margin: "0", borderLeft: "none", height: "4rem" }}
+                  className="timeBlocks"
+                >
+                  <span className="calendar_timeHeadings">{oneTime}:00</span>
+                </p>
+              );
+            })}
+          </div>
         </div>
 
         {day.map((oneDay: any) => {
@@ -101,41 +139,76 @@ export default function Day({
                   </span>
                 </Title>
               </div>
-              {timeArr.map((oneTime: number) => {
-                return (
-                  <div
-                    className="timeBlocks"
-                    style={{ height: "4rem", cursor: "pointer" }}
-                    onClick={() => handleOpenEventModal(oneDay, oneTime)}
-                  >
-                    <Title level={4} style={{ margin: "0" }}>
-                      {oneTime}
-                    </Title>
-                    <span>
-                      {savedEvents.length > 0 &&
-                        savedEvents.map((event: any) => {
-                          console.log(event);
-                          if (
-                            oneDay.format("DD-MM-YY") === event.dayMe &&
-                            oneTime === event.startTime
-                          ) {
-                            console.log("Wohooo", event);
-                            return (
-                              <span
-                                style={{ backgroundColor: event.labelColor }}
-                              >
-                                {event.title ? event.title : ""}
-                              </span>
-                            );
-                          }
-                        })}
-                    </span>
-                  </div>
-                );
-              })}
+              <div
+                style={{
+                  overflowY: scrollForTime ? "scroll" : "hidden",
+                }}
+                className="alltimesForEachDate"
+              >
+                {timeArr.map((oneTime: number) => {
+                  return (
+                    <div
+                      className="timeBlocks"
+                      style={{ height: "4rem", cursor: "pointer" }}
+                      onClick={() => handleOpenEventModal(oneDay, oneTime)}
+                    >
+                      <Title level={4} style={{ margin: "0" }}>
+                        {oneTime}
+                      </Title>
+                      <span>
+                        {savedEvents.length > 0 &&
+                          savedEvents.map((event: any) => {
+                            console.log(event);
+                            if (
+                              oneDay.format("DD-MM-YY") === event.dayMe &&
+                              oneTime === event.startTime
+                            ) {
+                              console.log("Wohooo", event);
+                              return (
+                                <span
+                                  style={{ backgroundColor: event.labelColor }}
+                                >
+                                  {event.title ? event.title : ""}
+                                </span>
+                              );
+                            }
+                          })}
+                      </span>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           );
         })}
+
+        <div>
+          <Title level={5} className="calendar_dayNames">
+            &nbsp;{" "}
+          </Title>
+          <Title level={2} className="dateDiv">
+            &nbsp;
+          </Title>
+          <div
+            ref={allTimesForEachDateDiv}
+            style={{
+              height: "70vh",
+              overflowY: "scroll",
+            }}
+            onClick={() => setScrollForTime(true)}
+          >
+            {timeArr.map((oneTime: number) => {
+              return (
+                <p
+                  style={{ margin: "0", borderLeft: "none", height: "4rem" }}
+                  className="timeBlocks"
+                >
+                  <span>&nbsp;</span>
+                </p>
+              );
+            })}
+          </div>
+        </div>
       </div>
 
       <EventModal

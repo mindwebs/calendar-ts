8b09975024f828fc74485f5b16530ff62878d2c9
Rohan Karmakar <krohan1202@gmail.com>
2022-07-05 02:11:11 +0530

    Update eventModal

--- src/components/EventModal/EventModal.tsx
+++ src/components/EventModal/EventModal.tsx
@@ -1,6 +1,13 @@
 import { useState, useEffect } from "react";
 import "./EventModal.css";
-import { Card, Button, TimePicker } from "antd";
+import { Card, Button, TimePicker, Input, Typography } from "antd";
+import {
+  CloseOutlined,
+  ClockCircleOutlined,
+  BgColorsOutlined,
+} from "@ant-design/icons";
+
+const { Title } = Typography;
 
 export default function EventModal({
   eventModalInfo,
@@ -71,57 +78,137 @@ export default function EventModal({
       <Card
         style={{
           display: openEventModal ? "block" : "none",
+          padding: "0",
         }}
         className="eventModalCard"
       >
-        <button onClick={() => handleCloseEventModal()}>Close</button>
-        <input
-          placeholder="Title"
+        <div>
+          <CloseOutlined
+            onClick={() => handleCloseEventModal()}
+            className="eventModal_closeBtn"
+          />
+        </div>
+        <Input
+          size="large"
+          placeholder="Add Title"
+          bordered={false}
+          className="eventModal_titleTextField"
           value={setUserEventInfo.title}
           onChange={(e) => handleNewEventDetails(e, "title")}
         />
-        <input
-          placeholder="Description"
+        <div style={{ display: "flex", margin: "0.8rem 0" }}>
+          <ClockCircleOutlined
+            style={{ margin: "auto 1rem auto 0", fontSize: "1.1rem" }}
+          />
+
+          <Title level={5} style={{ display: "block", margin: "auto 0" }}>
+            {eventModalInfo && eventModalInfo.oneDay.format("DD-MMMM-YYYY")}
+          </Title>
+        </div>
+
+        <div
+          style={{
+            paddingLeft: "1.95rem",
+            display: "flex",
+            margin: "0.8rem 0",
+            justifyContent: "space-between",
+          }}
+        >
+          {/* Start Time */}
+          <div>
+            {eventModalInfo && (
+              <select className="eventModal_timeHrSelect">
+                <option value={eventModalInfo.startTimeHr}>
+                  {eventModalInfo.startTimeHr}
+                </option>
+              </select>
+            )}
+            <TimePicker
+              format="mm"
+              placeholder="min"
+              className="eventModal_timePickerMinBox"
+              minuteStep={15}
+              onOk={(e) => handleNewEventDetails(e!.minute(), "startTimeMin")}
+            />
+          </div>
+          <div aria-label=" to ">
+            <span style={{ position: "relative", top: "0.25rem" }}>–</span>
+          </div>
+          {/* End Time */}
+          <div>
+            <select
+              value={setUserEventInfo.endTimeHr}
+              className="eventModal_timeHrSelect"
+              onChange={(e) =>
+                handleNewEventDetails(e.target.value, "endTimeHr")
+              }
+            >
+              {endTimeHrDropdownValues.map((eachHrTime: string) => {
+                return <option value={eachHrTime}>{eachHrTime}</option>;
+              })}
+            </select>
+            <TimePicker
+              format="mm"
+              minuteStep={15}
+              placeholder="min"
+              className="eventModal_timePickerMinBox"
+              onOk={(e) => handleNewEventDetails(e!.minute(), "endTimeMin")}
+            />
+          </div>
+        </div>
+        {/* Description */}
+        <Input
+          size="large"
+          placeholder="Add Description"
+          bordered={false}
+          style={{
+            width: "90%",
+            left: "10%",
+            margin: "0.5rem 0 1.5rem",
+            borderBottom: "1px solid #000",
+          }}
+          // className="eventModal_titleTextField"
           value={setUserEventInfo.description}
           onChange={(e) => handleNewEventDetails(e, "description")}
         />
-        {/* Start Time */}
-        <p>Start Time</p>
-        <p>{eventModalInfo && eventModalInfo.oneDay.format("DD-MMMM-YYYY")}</p>
-        <p>{eventModalInfo && eventModalInfo.startTimeHr}</p>
-        <TimePicker
-          format="mm"
-          minuteStep={15}
-          onOk={(e) => handleNewEventDetails(e!.minute(), "startTimeMin")}
-        />
-
-        {/* End Time */}
-        <p>End Time</p>
-        <select
-          value={setUserEventInfo.endTimeHr}
-          onChange={(e) => handleNewEventDetails(e.target.value, "endTimeHr")}
-        >
-          {endTimeHrDropdownValues.map((eachHrTime: string) => {
-            return <option value={eachHrTime}>{eachHrTime}</option>;
-          })}
-        </select>
-        <TimePicker
-          format="mm"
-          minuteStep={15}
-          onOk={(e) => handleNewEventDetails(e!.minute(), "endTimeMin")}
-        />
 
         {/* Colors */}
-        <select
-          value={setUserEventInfo.labelColor}
-          onChange={(e) => handleNewEventDetails(e, "labelColor")}
-        >
-          {labelColors.map((color: string) => {
-            return <option value={color}>{color}</option>;
-          })}
-        </select>
-        <Button onClick={() => addNewEvent()}>Add Event</Button>
+        <div style={{ display: "flex" }}>
+          <BgColorsOutlined
+            style={{ margin: "auto 1rem auto 0", fontSize: "1.35rem" }}
+          />
+          <Title
+            level={5}
+            style={{ display: "block", margin: "auto 1rem auto 0" }}
+          >
+            Choose color :{" "}
+          </Title>
+          <select
+            className="eventModal_timeHrSelect"
+            value={setUserEventInfo.labelColor}
+            onChange={(e) => handleNewEventDetails(e, "labelColor")}
+          >
+            {labelColors.map((color: string) => {
+              return <option value={color}>{color}</option>;
+            })}
+          </select>
+        </div>
+        <hr style={{ margin: "1rem 0" }} />
+        <div style={{ position: "relative", height: "2rem" }}>
+          <Button
+            style={{ position: "absolute", right: "0" }}
+            onClick={() => addNewEvent()}
+          >
+            Add Event
+          </Button>
+        </div>
       </Card>
+      <div
+        style={{
+          display: openEventModal ? "block" : "none",
+        }}
+        className="greyBg"
+      ></div>
     </>
   );
 }
